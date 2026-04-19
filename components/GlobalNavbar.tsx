d5644adc697f4ae19e8005f30a9a4c1dffce32cb
IvannnaTorous28 <s158555@ap.be>
2026-04-19 17:37:25 +0200

    Add files via upload

--- components/GlobalNavbar.tsx
+++ components/GlobalNavbar.tsx
@@ -1,6 +1,5 @@
 "use client"
-import Link from "next/link";
-import { FaRegUserCircle } from "react-icons/fa";
+
 import { useState } from "react"
 import SidebarSlider from "@/components/SidebarSlider"
 import { GiHamburgerMenu } from "react-icons/gi"
@@ -17,12 +16,7 @@ export default function GlobalNavbar() {
       >
         <GiHamburgerMenu size={24} />
       </button>
-      <Link
-         href="/login"
-         className="fixed top-4 right-4 z-50 p-2 text-gray-700 hover:text-gray-900"
-        >
-        <FaRegUserCircle size={28} />
-       </Link>
+
       {/* SidebarSlider component */}
       <SidebarSlider isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)}>
         <nav className="flex flex-col space-y-4">
@@ -30,9 +24,6 @@ export default function GlobalNavbar() {
           <a href="/about" className="hover:underline">About</a>
           <a href="/contact" className="hover:underline">Contact</a>
           <a href="/login" className="hover:underline">Login</a>
-          <a href="/page-1" className="hover:underline">Artikel-1</a>
-          <a href="/page-2" className="hover:underline">Artikel-2</a>
-          <a href="/page-3" className="hover:underline">Artikel-3</a>
         </nav>
       </SidebarSlider>
     </>
